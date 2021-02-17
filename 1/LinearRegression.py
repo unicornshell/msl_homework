bac8caa339eedd36b1dcd01f23419afd61e9fd9a
ddddkkkkdddd <yangwhite@outlook.com>
2021-02-17 23:09:24 +0800

    1

--- 1/LinearRegression.py
+++ 1/LinearRegression.py
@@ -10,12 +10,10 @@ from HelperClass.NeuralNet_1_1 import *
 file_name = "ner.npz"
 
 def ShowResult(net, reader):
-    # draw example points
     X,Y = reader.GetWholeTrainSamples()
     fig = plt.figure()
     ax = Axes3D(fig)
     ax.scatter(X[:,0],X[:,1],Y)
-    # draw fitting surface
     p = np.linspace(0,1)
     q = np.linspace(0,1)
     P,Q = np.meshgrid(p,q)
@@ -27,17 +25,12 @@ def ShowResult(net, reader):
 
 
 if __name__ == '__main__':
-    # data
     reader = DataReader_1_1(file_name)
     reader.ReadData()
     reader.NormalizeX()
-    # use this setting for the first time
     hp = HyperParameters_1_0(2, 1, eta=0.01, max_epoch=500, batch_size=10, eps = 1e-5)
-    # use this setting when you want to train more loops
-    #hp = HyperParameters_1_0(2, 1, eta=0.01, max_epoch=500, batch_size=10, eps = 1e-5)
     net = NeuralNet_1_1(hp)
     net.train(reader, checkpoint=0.1)
-    # inference
     x1 = 15
     x2 = 93
     x = np.array([x1,x2]).reshape(1,2)
